49f58fc9e35c53ed300a2e8f7bc53b541103efca
ozzi- <oz@oz-web.com>
2017-11-17 07:36:31 +0100

    Add files via upload

---
+++ Graylog Pin Fix.user.tamper.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Graylog Pin Fix
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Replaces input text field with password for OTP input
+// @match        https://graylog.*.ch/auth/token
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var tokenInput = document.getElementById("tokenValue");
+    tokenInput.type="password";
+})();
